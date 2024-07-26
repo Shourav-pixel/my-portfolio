26a82476e1b32db6ab35b07768cedeaf83ec3602
Shourav Chowdhury <chyshourav2@gmail.com>
2024-07-27 03:14:39 +0600

    about it

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -40,8 +40,7 @@ const About=()=>{
             </p>
             <p>
               If I need to define myself in one sentence that would be a family
-              person, father of a beautiful daughter, a sports fanatic,
-              photography enthusiast, and tech-obsessed!!!
+              person, Passionate about coding and problem solving enthusiast.
             </p>
           </div>
   
